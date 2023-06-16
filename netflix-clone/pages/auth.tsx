4ac1407dcab795744d209a2e32ca4f40b0af2d77
yagizharman <yagizharman4biz@gmail.com>
2023-06-16 17:47:45 +0300

    Login system implemented in three different ways. Tradional user info, using google authentication, github authentication

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,7 +1,13 @@
 import { useCallback, useState } from "react";
 import axios from 'axios';
 import Input from "../components/Input"
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 const Auth = () => {
+    const router = useRouter();
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -12,6 +18,20 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            })
+            router.push('/');
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }, [email, password, router]);
 
     const register = useCallback(async () => {
         try { 
@@ -20,11 +40,15 @@ const Auth = () => {
                 name,
                 password
             });
+            login();
         }
         catch(error) {
             console.log(error)
         }
-    }, [email,name,password]);
+    }, [email, name, password,login]);
+    
+ 
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -61,9 +85,37 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant==='login'? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant==='login' ? 'Login':'Sign up'}
                         </button>
+                        <div onClick={() => signIn('google', { callbackUrl: '/' })} className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div className="w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center
+                            justify-center
+                            cursor-pointer
+                            hover:opacity-80
+                            transition"
+                            >
+                                <FcGoogle size={ 30} />
+                            </div>
+                            <div onClick={ ()=>signIn('github',{callbackUrl:'/'})} className="w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center
+                            justify-center
+                            cursor-pointer
+                            hover:opacity-80
+                            transition"
+                            >
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             { variant==='login' ? 'First time using Netflix?' :'Already gave an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
