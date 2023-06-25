cdce0b40c542bae4e620f575466e339cd655b8bb
yagizharman <yagizharman4biz@gmail.com>
2023-06-25 14:21:28 +0300

    info modal is created and project finished.

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,8 +1,10 @@
 import Billboard from "@/components/Bilboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -27,8 +29,10 @@ export default function Home() {
   const { data: user } = useCurrentUser();
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
